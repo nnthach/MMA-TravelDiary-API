60323b23483ce8d4d2da9c9aba05836ada48b036
nnthach <nguyenngocthach2301@gmail.com>
2025-05-30 16:30:54 +0700

    Difference between dev and prod environment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,7 @@ import { StatusCodes } from "http-status-codes";
 const createUser = async (req, res, next) => {
   try {
     console.log("Request body in controller:", req.body);
+    
   } catch (error) {
     next(error);
   }
